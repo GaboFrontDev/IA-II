5fdf2bdd6c5578ab982affeda7d4ae115d24dc27
Gabriel Omar <omar.hinojoza13@gmail.com>
2019-08-26 21:04:27 -0500

    avances en perceptron

--- entrenamiento.py
+++ entrenamiento.py
@@ -1,7 +1,7 @@
 # CORRECCION DEL ERROR
 # Función de entrenamiento para encontrar los pesos que obedezcan a la salida deseada
 # Tenemos que calcular el error para encontrar la salida deseada " D "
-# E = D - Y
+# E = D_i - Y
 # Lo que nosotros queremos hacer es minimizar el vector de error cambiando nuestro
 # vector de pesos W.s
 # ------------------------
@@ -9,10 +9,21 @@
 # 1. iniciar vector W con valores aleatorios que
 #	 estén entre -1 y 1. EX:  W = [rand,rand]
 #
-# 2. Por cada entrada X_l en nuestros patrones
+# 2. Por cada entrada x en nuestros patrones
 # 	 de entrenamiento.
 # 	 Primero vamos a  calcular el error, después
 # 	 nuestros pesos nuevos.
 # 	 W[i] = W[i] + X[i]*e
 # 3. Si e != 0, regresamos a paso 2.
 D = [0, 0, 0, 1]  # Salida deseada
+
+
+# graph = [[x + 0, b - x*m]for x in steps]
+# for entrada, step in zip(entradas, steps):
+#     plt.scatter(entrada[0], entrada[1], color="blue" if step else "red")
+
+# m = peso1/peso2
+# b = umbral / peso2
+
+# plt.plot(graph[0], graph[3])
+# plt.show()
--- perceptron.py
+++ perceptron.py
@@ -1,29 +1,62 @@
 import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+
+def act_fun(val):
+    return 1 if val > 0 else 0
+
+
 entradas = [
     [0, 0],
     [0, 1],
     [1, 0],
     [1, 1]
 ]
+D = [0, 0, 0, 1]
+
 umbral = 1.5
+etha = 0.5
+peso1 = random.uniform(-1, 1)
+peso2 = random.uniform(-1, 1)
+print(peso1, peso2)
+errors = [False, False, False, False]
+salidas = [0, 0, 0, 0]
+converge = False
+idx = 0
+while False in errors:  # until converge
+    # val = np.dot([peso1, peso2], entradas[idx])
+    prod_punto = entradas[idx][0]*peso1 + entradas[idx][1]*peso2 - umbral
+    print("El resultado del producto punto es: ", prod_punto)
+    y = act_fun(prod_punto)
+    salidas[idx] = y
 
-""" PESOS PARA AND """
-peso1 = 1
-peso2 = 1
-""" PESOS PARA OR """
-# peso1 = 2
-# peso2 = 2
+    print("Deseado: ", D[idx])
+    print("Obtenido: ", y)
+    error = D[idx] - y
+    if error != 0:
+        print("Recalculando pesos...")
+        peso1 = peso1 + error * entradas[idx][0]
+        peso2 = peso2 + error * entradas[idx][1]
+        umbral = umbral + etha*error
+        print(peso1, peso2, umbral)
+        errors[idx] = False
+    else:
+        errors[idx] = True
+    idx += 1
+    if idx >= len(entradas):
+        m = peso1/peso2
+        b = umbral / peso2
+        graph = [[x + 0, b - x*m]for x in D]
+        print(y)
+        print(graph)
+        for entrada, step in zip(entradas, D):
+            plt.scatter(entrada[0], entrada[1],
+                        color="blue" if step else "red")
+        plt.plot(graph[0], graph[3])
+        idx = 0
 
-prod_punto = [a[0]*peso1 + a[1]*peso2 - umbral for a in entradas]
-steps = [(a > 0) for a in prod_punto]
+print("Entrenamiento terminado, se obtuvo: ", salidas)
 
-m = peso1/peso2
-b = umbral / peso2
-graph = [[x + 0, b - x*m]for x in steps]
-print(steps)
-print(graph)
-for entrada, step in zip(entradas, steps):
-    plt.scatter(entrada[0], entrada[1], color="blue" if step else "red")
 
-plt.plot(graph[0], graph[3])
 plt.show()
