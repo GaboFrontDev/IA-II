dd7dc1455a5325751117dec022a947926b5ae93d
Gabriel Omar <omar.hinojoza13@gmail.com>
2019-08-28 15:57:28 -0500

    avances perceptron

---
+++ neurona_base.py
@@ -0,0 +1,6 @@
+class Neurona:
+    "This is my second class"
+    pesos = []
+
+    def func(self):
+        print('Hello')
--- perceptron.py
+++ perceptron.py
@@ -9,54 +9,52 @@ def act_fun(val):
 
 entradas = [
     [0, 0],
-    [0, 1],
     [1, 0],
+    [0, 1],
     [1, 1]
 ]
 D = [0, 0, 0, 1]
+# D = [0, 1, 1, 1]
 
-umbral = 1.5
-etha = 0.5
+umbral = 0
+etha = 0.01
 peso1 = random.uniform(-1, 1)
 peso2 = random.uniform(-1, 1)
-print(peso1, peso2)
-errors = [False, False, False, False]
-salidas = [0, 0, 0, 0]
-converge = False
+did_error = [True, True, True, True]
+salidas = [0, 1, 1, 0]
 idx = 0
-while False in errors:  # until converge
-    # val = np.dot([peso1, peso2], entradas[idx])
-    prod_punto = entradas[idx][0]*peso1 + entradas[idx][1]*peso2 - umbral
-    print("El resultado del producto punto es: ", prod_punto)
+
+
+while True in did_error:  # until converge
+    prod_punto = (entradas[idx][0]*peso1 + entradas[idx][1]*peso2) + umbral
     y = act_fun(prod_punto)
     salidas[idx] = y
-
-    print("Deseado: ", D[idx])
-    print("Obtenido: ", y)
     error = D[idx] - y
     if error != 0:
         print("Recalculando pesos...")
-        peso1 = peso1 + error * entradas[idx][0]
-        peso2 = peso2 + error * entradas[idx][1]
+        peso1 = peso1 + etha * error * entradas[idx][0]
+        peso2 = peso2 + etha * error * entradas[idx][1]
         umbral = umbral + etha*error
-        print(peso1, peso2, umbral)
-        errors[idx] = False
+        did_error[idx] = True
     else:
-        errors[idx] = True
+        did_error[idx] = False
     idx += 1
     if idx >= len(entradas):
-        m = peso1/peso2
-        b = umbral / peso2
-        graph = [[x + 0, b - x*m]for x in D]
-        print(y)
-        print(graph)
+        x1 = np.linspace(-3, 3, 20)
+        x2 = (-peso1/peso2) * x1 - (umbral / peso2)
+        # print("salida: ", y)
         for entrada, step in zip(entradas, D):
             plt.scatter(entrada[0], entrada[1],
                         color="blue" if step else "red")
-        plt.plot(graph[0], graph[3])
+
+        plt.plot(x1, x2, color="red")
+        plt.pause(0.00005)
         idx = 0
+        ax = plt.gca()
+        del ax.lines[0]
 
-print("Entrenamiento terminado, se obtuvo: ", salidas)
 
-
+x1 = np.linspace(-3, 3, 20)
+x2 = (-peso1/peso2) * x1 - (umbral / peso2)
+plt.plot(x1, x2, color="blue")
 plt.show()
